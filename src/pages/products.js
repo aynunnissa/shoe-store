29decb07f700a0e3dea2725bd91c461189c68b53
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2022-06-28 11:29:06 +0700

    Update products page layout

--- src/pages/products.js
+++ src/pages/products.js
@@ -26,12 +26,7 @@ const Products = () => {
   }, [filterProduct]);
 
   return (
-    <Grid container sx={{ minHeight: "100vh" }}>
-      {/* <Grid item xs={3}>
-        <Box sx={{ height: "100%", backgroundColor: "#E5E5E5" }}>
-          <Filter />
-        </Box>
-      </Grid> */}
+    <Grid container sx={{ minHeight: "100vh" }} px={2} py={4} spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h5" component="h1">
           Men's {type ? type[0].toUpperCase() + type.slice(1) : ""} Shoes (
